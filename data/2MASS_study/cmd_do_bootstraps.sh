ce633a49b03382dc180a1c1b5b625b7302a0653d
Matthew Bellis <matthew.bellis@gmail.com>
2012-01-25 07:10:58 -0800

    Updated a few things.

--- data/2MASS_study/cmd_do_bootstraps.sh
+++ data/2MASS_study/cmd_do_bootstraps.sh
@@ -10,6 +10,8 @@ set tag = "40k_bins6"
 
 @ max = 10
 
+cd ~/CUDA/galaxy_correlation_function/data/2MASS_study
+
 while ( $i < 10 )
 
     set datafile = `printf "$data_dir""/sample_%04d.csv" $i`
@@ -18,18 +20,15 @@ while ( $i < 10 )
     #echo $datafile
     #echo $mcfile
 
-        set outfile = `printf "$outdir""/output_DD_"$tag"_%04d.dat" $i`
-        echo time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $datafile $outfile
-        time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $datafile $outfile
-        set outfile = `printf "$outdir""/output_RR_"$tag"_%04d.dat" $i`
-        echo time ../../CUDA_code/Calculate_arc_length_two_datasets $mcfile  $mcfile $outfile
-        time ../../CUDA_code/Calculate_arc_length_two_datasets $mcfile  $mcfile $outfile
-        set outfile = `printf "$outdir""/output_DR_"$tag"_%04d.dat" $i`
-        echo time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $mcfile $outfile
-        time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $mcfile $outfile
-
-        #time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile $datafile out_RR_$tag.dat
-        #time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile $datafile out_DR_$tag.dat
+    set outfile = `printf "$outdir""/output_DD_"$tag"_%04d.dat" $i`
+    echo time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $datafile $outfile
+    time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $datafile $outfile
+    set outfile = `printf "$outdir""/output_RR_"$tag"_%04d.dat" $i`
+    echo time ../../CUDA_code/Calculate_arc_length_two_datasets $mcfile  $mcfile $outfile
+    time ../../CUDA_code/Calculate_arc_length_two_datasets $mcfile  $mcfile $outfile
+    set outfile = `printf "$outdir""/output_DR_"$tag"_%04d.dat" $i`
+    echo time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $mcfile $outfile
+    time ../../CUDA_code/Calculate_arc_length_two_datasets $datafile  $mcfile $outfile
 
     @ i++
 
