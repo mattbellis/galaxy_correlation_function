3bac63680ae18520f15319dc28793c8beb839b89
Matthew Bellis <matthew.bellis@gmail.com>
2012-03-27 16:56:36 -0500

    Added command-line parser to code.

--- CUDA_code/Calculate_arc_length_two_datasets.cu
+++ CUDA_code/Calculate_arc_length_two_datasets.cu
@@ -2,6 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<math.h>
+#include <unistd.h>
 
 #include <cuda_runtime.h>
 //#include <cutil_inline.h>
@@ -66,21 +67,21 @@ __global__ void distance(float *a0, float *d0, float *a1, float *d1, int xind, i
         if (do_calc)
         {
             a_diff = a1[i] - alpha;
-            
+
             sin_a_diff = sin(a_diff);
             cos_a_diff = cos(a_diff);
-  
+
             sin_d1 = sin(d1[i]);
             cos_d1 = cos(d1[i]);
- 
+
             mult1 = cos_d1 * cos_d1 * sin_a_diff * sin_a_diff;
             mult2 = cos_d0 * sin_d1 - sin_d0 * cos_d1 * cos_a_diff;
             mult2 = mult2 * mult2;
-           
+
             numer = sqrt(mult1 + mult2); 
-       
+
             denom = sin_d0 *sin_d1 + cos_d0 * cos_d1 * cos_a_diff;
-            
+
             //dist = atan(num);  
             dist = atan2(numer,denom);  
             dist *= CONV_FACTOR;  // Convert to degrees
@@ -90,26 +91,26 @@ __global__ void distance(float *a0, float *d0, float *a1, float *d1, int xind, i
             else if(dist >= HIST_MAX)
                 bin_index = NUM_BIN + 1;
             else
+            {
+                //bin_index = int(((dist - HIST_MIN) * NUM_BIN / HIST_MAX) +1);    
+                bin_index = 0;
+                for (int j=0;j<NUM_BIN-1;j++)
                 {
-                    //bin_index = int(((dist - HIST_MIN) * NUM_BIN / HIST_MAX) +1);    
-                    bin_index = 0;
-                    for (int j=0;j<NUM_BIN-1;j++)
+                    //bin_index = 5;
+                    //if (dist>=0.1*j && dist<0.1*(j+1))
+                    //if (dist>=dev_bin_edges[j] && dist<dev_bin_edges[j+1])
+                    if (dist>=bin_edges[j] && dist<bin_edges[j+1])
                     {
-                        //bin_index = 5;
-                        //if (dist>=0.1*j && dist<0.1*(j+1))
-                        //if (dist>=dev_bin_edges[j] && dist<dev_bin_edges[j+1])
-                        if (dist>=bin_edges[j] && dist<bin_edges[j+1])
-                        {
-                            bin_index = j+1;
-                            break;
-                        }
+                        bin_index = j+1;
+                        break;
                     }
                 }
+            }
 
             offset = ((NUM_BIN+2)*thread_idx);
             bin_index += offset;
 
-           dev_hist[bin_index]++;
+            dev_hist[bin_index]++;
 
         }
     }
@@ -120,6 +121,126 @@ __global__ void distance(float *a0, float *d0, float *a1, float *d1, int xind, i
 ////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
+    // Needed for parsing command-line arguments.
+    extern char *optarg;
+    extern int optind, optopt, opterr;
+    int c;
+    char *filename;
+    char *outfilename = NULL;
+    char defaultoutfilename[256];
+    sprintf(defaultoutfilename,"default_out.dat");
+    char *binning_filename = NULL;
+    FILE *binning_file = NULL;
+
+    int size;
+
+    while ((c = getopt(argc, argv, "ab:f:o:")) != -1) {
+        switch(c) {
+            case 'a':
+                printf("a is set\n");
+                break;
+            case 'b':
+                binning_filename = optarg;
+                printf("Using binning information from file: %s\n",binning_filename);
+                break;
+            case 'f':
+                filename = optarg;
+                printf("filename is %s\n", filename);
+                break;
+            case 'o':
+                outfilename = optarg;
+                printf("Output filename is %s\n", outfilename);
+                break;
+            case '?':
+                printf("unknown arg %c\n", optopt);
+                break;
+        }
+    }
+
+    if (argc < 2)
+    {
+
+        printf("\nMust pass in at least two input files on command line!\n");
+        printf("\nUsage: ", argv[0] );
+        //printf(" <cluster_data file> <distances file> \n\n");
+        exit(1);
+    }
+
+    // Set a default output file name, if none was passed in on the 
+    // command line.
+    if (outfilename == NULL) 
+    {
+        outfilename = defaultoutfilename;
+        printf("Output filename is %s\n", outfilename);
+    }
+
+    //printf ("%d\n", optind);
+    //printf ("%d\n", argc);
+    //printf ("%d\n", optind);
+    //printf ("%s\n", argv[optind]);
+    //printf ("%s\n", argv[optind+1]);
+
+    FILE *infile0, *infile1, *outfile ;
+    infile0 = fopen(argv[optind],"r");
+    infile1 = fopen(argv[optind+1],"r");
+    //outfile = fopen(argv[3], "w");
+
+    printf("Opening input file 0: %s\n",argv[optind]);
+    printf("Opening input file 1: %s\n",argv[optind+1]);
+    //printf("Outfilename: %s\n",outfilename);
+    outfile = fopen(outfilename, "w");
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Check to see if the two files are actually the same file.
+    // This is the case for the DD and RR calculations and change slightly
+    // the exact calculations being performed.
+    ////////////////////////////////////////////////////////////////////////////
+    bool two_different_files = 1;
+    if (strcmp(argv[optind],argv[optind+1])==0)
+    {
+        two_different_files = 0;
+        printf("Using the same file!\n");
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // Now get the info from the device.
+    ////////////////////////////////////////////////////////////////////////////
+    int deviceCount = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+    if (error_id != cudaSuccess) {
+        printf( "cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id) );
+    }
+    // This function call returns 0 if there are no CUDA capable devices.
+    if (deviceCount == 0)
+        printf("There is no device supporting CUDA\n");
+    else
+        printf("Found %d CUDA Capable device(s)\n", deviceCount);
+
+
+    int dev, driverVersion = 0, runtimeVersion = 0;
+    for (dev = 0; dev < deviceCount; ++dev) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+        printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
+
+        printf("  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
+                (float)deviceProp.totalGlobalMem/1048576.0f, (unsigned long long) deviceProp.totalGlobalMem);
+
+
+        printf("  Warp size:                                     %d\n", deviceProp.warpSize);
+        printf("  Maximum number of threads per block:           %d\n", deviceProp.maxThreadsPerBlock);
+        printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+                deviceProp.maxThreadsDim[0],
+                deviceProp.maxThreadsDim[1],
+                deviceProp.maxThreadsDim[2]);
+        printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+                deviceProp.maxGridSize[0],
+                deviceProp.maxGridSize[1],
+                deviceProp.maxGridSize[2]);
+
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////
 
     float *d_alpha0, *d_delta0;
     float *h_alpha0, *h_delta0;
@@ -128,55 +249,64 @@ int main(int argc, char **argv)
     float *h_alpha1, *h_delta1;
 
     //float *d_bin_edges;
-    //float *h_bin_edges;
+    float *h_bin_edges;
 
-    int NUM_PARTICLES;
-
-    if (argc < 4)
-    {
-
-        printf("\nMust pass in cluster_data file  on command line!\n");
-        printf("\nUsage: ", argv[0] );
-        printf(" <cluster_data file> <distances file> \n\n");
-        exit(1);
-    }
-
-    FILE *infile0, *infile1, *outfile ;
-    infile0 = fopen(argv[1],"r");
-    infile1 = fopen(argv[2],"r");
-    outfile = fopen(argv[3], "w");
-
-    bool two_different_files = 1;
-    if (strcmp(argv[1],argv[2])==0)
-    {
-        two_different_files = 0;
-        printf("Using the same file!\n");
-    }
+    int NUM_GALAXIES;
 
     //////////////////////////////////////////////////////////////////////
     // Read in the file that defines the bin edges.
     ////////////////////////////////////////////////////////////////////////////
 
+    int default_nbins = 27;
+    float default_bin_edges[NUM_BIN] = {0.0000,0.001000,0.001585,0.002512,0.003981,0.006310,0.010000,0.015849,0.025119,0.039811,0.063096,0.100000,0.158489,0.251189,0.398107,0.630957,1.000000,1.584893,2.511886,3.981072,6.309573,10.000000,15.848932,25.118864,39.810717,63.095734,100.000000};
+
+    int nbins=0;
+    float temp_bin_edges[4096];
+    //FILE *binning_file = NULL;
+    if (binning_filename != NULL)
+    {
+        printf("BBBBBBBBBBBB: %s\n",binning_filename);
+        binning_file = fopen(binning_filename,"r");
+
+        while(fscanf(binning_file, "%f", &temp_bin_edges[nbins])!=EOF)
+        {
+            nbins++;
+        }
+
+        // Copy over the temp bin edges into the host array.
+        size = nbins * sizeof(float);    
+        h_bin_edges = (float*)malloc(size);
+        for (int i=0;i<nbins;i++)
+        {
+            h_bin_edges[i] = temp_bin_edges[i];
+            printf("h_bin_edges: %3d %f\n",i,h_bin_edges[i]);
+        }
+    }
+    else
+    {
+        // No file containing bin edges was passed in on the 
+        // command line, so use the defaults.
+        nbins = default_nbins;
+        size = nbins * sizeof(float);    
+        h_bin_edges = (float*)malloc(size);
+        for (int i=0;i<nbins;i++)
+        {
+            h_bin_edges[i] = default_bin_edges[i];
+            printf("h_bin_edges: %3d %f\n",i,h_bin_edges[i]);
+        }
+    }
+
     /*
-    FILE *binning_file = NULL;
-    if (argc>4)
-    {
-        binning_file = fopen(argv[4],"r");
-    }
+       int size = nbins * sizeof(float);    
+       printf("SIZE0 # GALAXIES: %d\n",nbins+1);
 
-    int nbins;
-    fscanf(binning_file, "%d", &nbins);
+       h_bin_edges = (float*)malloc(size);
 
-    int size = nbins * sizeof(float);    
-    printf("SIZE0 # particles: %d\n",nbins+1);
-
-    h_bin_edges = (float*)malloc(size);
-
-    for(int i=0; i<nbins; i++)
-    {
-        fscanf(infile0, "%f", &h_bin_edges[i]);
-    }
-    */
+       for(int i=0; i<nbins; i++)
+       {
+       fscanf(infile0, "%f", &h_bin_edges[i]);
+       }
+     */
 
     //////////////////////////////////////////////////////////////////////
     // Read in the cluster_data file
@@ -188,39 +318,39 @@ int main(int argc, char **argv)
     ////////////////////////////////////////////////////////////////////////////
     // Read in the first file
     ////////////////////////////////////////////////////////////////////////////
-    
+
     fscanf(infile0, "%s %s %s", &axis_titles, &dummy, &axis_titles);
-    fscanf(infile0, "%d", &NUM_PARTICLES);
-   
-    int size = NUM_PARTICLES * sizeof(float);    
-    printf("SIZE0 # particles: %d\n",NUM_PARTICLES);
+    fscanf(infile0, "%d", &NUM_GALAXIES);
+
+    size = NUM_GALAXIES * sizeof(float);    
+    printf("SIZE 0 # GALAXIES: %d\n",NUM_GALAXIES);
 
     h_alpha0 = (float*)malloc(size);
     h_delta0 = (float*)malloc(size);
 
-    for(int i=0; i<NUM_PARTICLES; i++)
+    for(int i=0; i<NUM_GALAXIES; i++)
     {
         fscanf(infile0, "%f %s %f %s ", &h_alpha0[i], &dummy, &h_delta0[i], &dummy);
-       //fscanf(infile, "%f%s %f ", &h_alpha[i], &dummy, &h_delta[i]);
-       //printf("%e %s %e\n", h_alpha0[i], dummy, h_delta0[i]);
+        //fscanf(infile, "%f%s %f ", &h_alpha[i], &dummy, &h_delta[i]);
+        //printf("%e %s %e\n", h_alpha0[i], dummy, h_delta0[i]);
     }
 
     ////////////////////////////////////////////////////////////////////////////
     // Read in the second file
     ////////////////////////////////////////////////////////////////////////////
-    
+
     fscanf(infile1, "%s %s %s", &axis_titles, &dummy, &axis_titles);
-    fscanf(infile1, "%d", &NUM_PARTICLES);
-    printf("SIZE1 # particles: %d\n",NUM_PARTICLES);
-   
+    fscanf(infile1, "%d", &NUM_GALAXIES);
+    printf("SIZE 1 # GALAXIES: %d\n",NUM_GALAXIES);
+
     h_alpha1 = (float*)malloc(size);
     h_delta1 = (float*)malloc(size);
 
-    for(int i=0; i<NUM_PARTICLES; i++)
+    for(int i=0; i<NUM_GALAXIES; i++)
     {
         fscanf(infile1, "%f %s %f %s ", &h_alpha1[i], &dummy, &h_delta1[i], &dummy);
-       //fscanf(infile, "%f%s %f ", &h_alpha[i], &dummy, &h_delta[i]);
-       // printf("%e %s %e\n", h_alpha[i], dummy, h_delta[i]);
+        //fscanf(infile, "%f%s %f ", &h_alpha[i], &dummy, &h_delta[i]);
+        //printf("%e %s %e\n", h_alpha1[i], dummy, h_delta1[i]);
     }
 
     ////////////////////////////////////////////////////////////////////////////
@@ -235,19 +365,21 @@ int main(int argc, char **argv)
     // Log binning
     //float h_bin_edges[30] = {0.001000,0.001585,0.002512,0.003981,0.006310,0.010000,0.010000,0.015849,0.025119,0.039811,0.063096,0.100000,0.100000,0.158489,0.251189,0.398107,0.630957,1.000000,1.000000,1.584893,2.511886,3.981072,6.309573,10.000000,10.000000,15.848932,25.118864,39.810717,63.095734,100.000000};
 
+    ////////////////////////////////////////////////////////////////////////////
     // For 27 bins
-    float h_bin_edges[NUM_BIN] = {0.0000,0.001000,0.001585,0.002512,0.003981,0.006310,0.010000,0.015849,0.025119,0.039811,0.063096,0.100000,0.158489,0.251189,0.398107,0.630957,1.000000,1.584893,2.511886,3.981072,6.309573,10.000000,15.848932,25.118864,39.810717,63.095734,100.000000};
+    ////////////////////////////////////////////////////////////////////////////
+    //float h_bin_edges[NUM_BIN] = {0.0000,0.001000,0.001585,0.002512,0.003981,0.006310,0.010000,0.015849,0.025119,0.039811,0.063096,0.100000,0.158489,0.251189,0.398107,0.630957,1.000000,1.584893,2.511886,3.981072,6.309573,10.000000,15.848932,25.118864,39.810717,63.095734,100.000000};
 
     // For 37 bins
     //float h_bin_edges[NUM_BIN] = {0.0000,0.001000,0.001389,0.001931,0.002683,0.003728,0.005179,0.007197,0.010000,0.013895,0.019307,0.026827,0.037276,0.051795,0.071969,0.100000,0.138950,0.193070,0.268270,0.372759,0.517947,0.719686,1.000000,1.389495,1.930698,2.682696,3.727594,5.179475,7.196857,10.000000,13.894955,19.306977,26.826958,37.275937,51.794747,71.968567,100.000000};
 
     /*
-    for (int i=0;i<NUM_BIN;i++)
-    {
-        printf("%d %f\n",i,h_bin_edges[i]);
-    }
-    printf("\n");
-    */
+       for (int i=0;i<NUM_BIN;i++)
+       {
+       printf("%d %f\n",i,h_bin_edges[i]);
+       }
+       printf("\n");
+     */
     float *dev_bin_edges;
     cudaMalloc((void **) &dev_bin_edges, (NUM_BIN*sizeof(float)));
     cudaMemset(dev_bin_edges, 0, NUM_BIN);
@@ -274,7 +406,7 @@ int main(int argc, char **argv)
     ////////////////////////////////////////////////////////////////////////////
     dim3 grid, block;
     grid.x =100;
-    block.x = SUBMATRIX_SIZE/grid.x; //NUM_PARTICLES/block.x;
+    block.x = SUBMATRIX_SIZE/grid.x; //NUM_GALAXIES/block.x;
     ////////////////////////////////////////////////////////////////////////////
 
     cudaMalloc((void **) &d_alpha0, size );
@@ -282,7 +414,7 @@ int main(int argc, char **argv)
 
     cudaMalloc((void **) &d_alpha1, size );
     cudaMalloc((void **) &d_delta1, size );
-    
+
     // Check to see if we allocated enough memory.
     if (0==d_alpha0 || 0==d_delta0 || 0==d_alpha1 || 0==d_delta1 || 0==dev_hist)
     {
@@ -303,41 +435,41 @@ int main(int argc, char **argv)
     cudaMemcpy(d_delta1, h_delta1, size, cudaMemcpyHostToDevice );
 
     int x, y;
-    int num_submatrices = NUM_PARTICLES / SUBMATRIX_SIZE;
+    int num_submatrices = NUM_GALAXIES / SUBMATRIX_SIZE;
 
 
     int bin_index = 0;
     for(int k = 0; k < num_submatrices; k++)
     {
         y = k*SUBMATRIX_SIZE;
-//        printf("%d %d\n",k,y);
+        //        printf("%d %d\n",k,y);
         for(int j = 0; j < num_submatrices; j++)
         {
-                x = j *SUBMATRIX_SIZE; 
+            x = j *SUBMATRIX_SIZE; 
 
-                //printf("----\n");
-                //printf("%d %d\t\t%d %d\n",k,y,j,x);
-                //printf("----\n");
+            //printf("----\n");
+            //printf("%d %d\t\t%d %d\n",k,y,j,x);
+            //printf("----\n");
 
-                cudaMemset(dev_hist,0,size_hist_bytes);
+            cudaMemset(dev_hist,0,size_hist_bytes);
 
-                distance<<<grid,block>>>(d_alpha0, d_delta0,d_alpha1, d_delta1, x, y, dev_hist, dev_bin_edges, two_different_files);
-                cudaMemcpy(hist, dev_hist, size_hist_bytes, cudaMemcpyDeviceToHost);
+            distance<<<grid,block>>>(d_alpha0, d_delta0,d_alpha1, d_delta1, x, y, dev_hist, dev_bin_edges, two_different_files);
+            cudaMemcpy(hist, dev_hist, size_hist_bytes, cudaMemcpyDeviceToHost);
 
 
-                for(int m=0; m<size_hist; m++)
-                {
+            for(int m=0; m<size_hist; m++)
+            {
 
-                    bin_index = m%(NUM_BIN+2);
-                    //if(bin_index == 0)
-                        //printf("\n");
-
-                    //printf("%3i:%3i ", m, hist[m]);
-                    //printf("%3i ", hist[m]);
-
-                    hist_array[bin_index] += hist[m];
-                }    
+                bin_index = m%(NUM_BIN+2);
+                //if(bin_index == 0)
                 //printf("\n");
+
+                //printf("%3i:%3i ", m, hist[m]);
+                //printf("%3i ", hist[m]);
+
+                hist_array[bin_index] += hist[m];
+            }    
+            //printf("\n");
         }  
     }
 
@@ -348,19 +480,19 @@ int main(int argc, char **argv)
     fprintf(outfile, "%s %s\n", "Angular Distance(radians)","Number of Entries");      
     for(int k=0; k<NUM_BIN+1; k++)
     {
-       //bins_mid = bin_width*(k - 0.5);
+        //bins_mid = bin_width*(k - 0.5);
 
-       float lo = h_bin_edges[k];
-       float hi = h_bin_edges[k+1];
+        float lo = h_bin_edges[k];
+        float hi = h_bin_edges[k+1];
 
-       bins_mid = (hi+lo)/2.0;
+        bins_mid = (hi+lo)/2.0;
 
-       fprintf(outfile, "%.3e %s %lu \n", bins_mid, ",",  hist_array[k]);
-       total += hist_array[k];
+        fprintf(outfile, "%.3e %s %lu \n", bins_mid, ",",  hist_array[k]);
+        total += hist_array[k];
 
     }
     printf("total: %lu \n", total);
-    
+
     fclose(infile0);
     fclose(infile1);
     fclose(outfile);
