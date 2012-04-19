374c1f30eb68859a2b11be967d7dd6de022a9a58
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-19 18:34:11 -0500

    Added.

---
+++ data/2MASS_study/cmds_for_nncompare.sh
@@ -0,0 +1,3 @@
+time ../../CUDA_code/Calculate_arc_length_two_datasets /home/bellis/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcmin.dat /home/bellis/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcmin.dat -o logbinning_100k_data_data_minutes_as_input.dat -w 0.05 -L 1.00 -N187 -l 1
+
+time ../../CUDA_code/Calculate_arc_length_two_datasets /home/bellis/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcsec.dat /home/bellis/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcsec.dat -o logbinning_100k_data_data_seconds_as_input.dat -w 0.05 -L 1.00 -N187 -l 1
