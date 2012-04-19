e60b91dfc0d2973ff695f06500e40a07efa4dea0
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-19 17:26:45 -0500

    Added some stuff to cmds_new.sh

--- data/2MASS_study/cmds_new.sh
+++ data/2MASS_study/cmds_new.sh
@@ -5,3 +5,7 @@ time ../../CUDA_code/Calculate_arc_length_two_datasets 2MASS_40k_RA_Dec.dat 2MAS
 time ../../CUDA_code/Calculate_arc_length_two_datasets ~/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec.dat ~/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec.dat -o logbinning_test.dat -w 0.5 -L 0.001 -N25 -l 1
 time ../../CUDA_code/Calculate_arc_length_two_datasets ../2MASS_study/mc100k_ra_dec.dat ../2MASS_study/mc100k_ra_dec.dat -o logbinning_test_mcmc.dat -w 0.5 -L 0.001 -N25 -l 1
 time ../../CUDA_code/Calculate_arc_length_two_datasets ~/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec.dat ../2MASS_study/mc100k_ra_dec.dat -o logbinning_test_mc_data.dat -w 0.5 -L 0.001 -N25 -l 1
+
+
+# Testing arcsecond calculations
+time ../../CUDA_code/Calculate_arc_length_two_datasets ~/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcseconds.dat ~/Work/Astronomy/catalogs/Wechsler/wg100k_ra_dec_arcseconds.dat -o logbinning_test_data_data.dat -w 0.5 -L 0.0001 -N35 -l 1
