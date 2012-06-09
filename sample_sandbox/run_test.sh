1e5aed2005c997459af343eb358c610318729112
Matthew Bellis <matthew.bellis@gmail.com>
2012-06-09 09:27:23 -0700

    Changed test code.

--- sample_sandbox/run_test.sh
+++ sample_sandbox/run_test.sh
@@ -3,15 +3,17 @@
 #set executable = '../CUDA_code/Calculate_arc_length_two_datasets'
 set executable = '../CUDA_code/Calculate_arc_length_two_datasets_shared'
 
-set mc   = '../sample_input_data/mc100k_ra_dec_arcmin.dat'
-set data = '../sample_input_data/wg100k_ra_dec_arcmin.dat'
+set ngals = $1
+
+set mc   = '../sample_input_data/mc'$ngals'k_ra_dec_arcmin.dat'
+set data = '../sample_input_data/wg'$ngals'k_ra_dec_arcmin.dat'
 
 #set global_params = '-w 0.05 -L 1.00 -N187 -l 1 -m'
 set global_params = '-w 0.05 -L 1.00 -l 1 -m'
 
-time $executable $data $data $global_params -o logbinning_100k_data_data_arcmin.dat 
+time $executable $data $data $global_params -o logbinning_"$ngals"k_data_data_arcmin.dat 
 echo "\n#####################\n"
-time $executable $mc $mc     $global_params -o logbinning_100k_mc_mc_arcmin.dat 
+time $executable $mc $mc     $global_params -o logbinning_"$ngals"k_mc_mc_arcmin.dat 
 echo "\n#####################\n"
-time $executable $data $mc   $global_params -o logbinning_100k_data_mc_arcmin.dat 
+time $executable $data $mc   $global_params -o logbinning_"$ngals"k_data_mc_arcmin.dat 
 echo "\n#####################\n"
